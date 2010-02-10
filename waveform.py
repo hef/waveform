adaf3e2764ea20e20fcb9c74c9944d9248d74bd4
hef@pbrfrat.com <hef@pbrfrat.com>
2010-02-09 23:40:32 -0600

    added option parsing

--- waveform.py
+++ waveform.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python
 import sys
 import Gnuplot
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--format", dest="format", help="Format to use", default="svg")
+parser.add_option("-i", "--input", dest="input", help="Input File")
+parser.add_option("-o", "--output", dest="output", help="Output File")
+(options, args) = parser.parse_args()
+print options
+print options.input
+formatString = {
+	'svg': "svg enhanced size 1000 1000 fname \"Times\" fsize 10",
+	'epslatex': "epslatex"
+}
 
 #calculate number of plots to make
-log = open(sys.argv[1])
+log = open(options.input)
 header = log.readline().strip()
 columns = len(header.split("\t"))
 
 waveform = Gnuplot.Gnuplot(debug=1)
-waveform("set terminal svg enhanced size 1000 1000 fname \"Times\" fsize 10")
+#waveform("set terminal svg enhanced size 1000 1000 fname \"Times\" fsize 10")
+waveform("set terminal %s"%(formatString[options.format]))
+waveform("set output %s"%(options.output) )
 waveform("set key autotitle columnhead")
 waveform("set border 3")
 waveform("set multiplot layout %d,1"%(columns))
-for plotIndex in range(1,columns):
-	waveform.plot("'%s' using %d with steps"%(sys.argv[1],plotIndex))
+for plotIndex in range(1,columns+1):
+	waveform.plot("'%s' using %d with steps"%(options.input,plotIndex))
 waveform("unset multiplot")
 
 
